013da3ede6fd3383a87dc3d4e07f59375e49f275
qord456 <85505034+qord456@users.noreply.github.com>
2022-03-23 05:55:48 +0700

    Add file for homework from 21032022

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import './albumData';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h1>CREATE PLAYLIST</h1>
+        <div className="form">
+        <p>Title</p>
+          <input id="inputTitle" placeholder="Insert playlist title" />
+          <p>Description</p>
+          <textarea
+            id="inputDescription"
+            placeholder="Insert playlist description"
+          ></textarea>
+          <p></p>
+          <button id="submit" type="submit">Create</button>
+        </div>
+        <div>
+          <div className="album">
+          <img src="https://c.tenor.com/JniFp1Q_5ToAAAAC/soodam-secret-number.gif"></img>
+          <p>Title: Who Dis'</p>
+          <p>Artist: 시크릿넘버</p>
+          <button id='tombol'>Select</button>
+        </div>
+        </div>
+        
+      </div>
     </div>
   );
 }
+
+
 
 export default App;
