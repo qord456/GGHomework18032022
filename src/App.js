67796a17e4ec139b267b7fc2c0089c25b23e8f95
qord456 <85505034+qord456@users.noreply.github.com>
2022-03-25 16:10:15 +0700

    Homework from 23032022

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import logo from './logo.svg';
 import './App.css';
 import './albumData';
+import MyExtract from './components/components'
+import MyExtractingAfter from './components/components';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <div className="container">
         <h1>CREATE PLAYLIST</h1>
         <div className="form">
-        <p>Title</p>
+          <p>Title</p>
           <input id="inputTitle" placeholder="Insert playlist title" />
           <p>Description</p>
           <textarea
@@ -18,17 +19,28 @@ function App() {
           <p></p>
           <button id="submit" type="submit">Create</button>
         </div>
-        <div>
-          <div className="album">
-          <img src="https://c.tenor.com/JniFp1Q_5ToAAAAC/soodam-secret-number.gif"></img>
-          <p>Title: Who Dis'</p>
-          <p>Artist: 시크릿넘버</p>
-          <button id='tombol'>Select</button>
-        </div>
-        </div>
         
       </div>
+      <div className="container">
+        <div className="albumLists">
+          <div className="album">
+            <img src="https://c.tenor.com/JniFp1Q_5ToAAAAC/soodam-secret-number.gif"></img>
+            <p>Title: Who Dis'</p>
+            <p>Artist: 시크릿넘버</p>
+            <button id='tombol'>Select</button>
+          </div>
+          <MyExtractingAfter
+            artistName="Queen"
+            nameHref="https://open.spotify.com/artist/1dfeR4HaWDbWqFHLkxsg1d"
+            albumPictSrc="https://i.scdn.co/image/ab67616d0000b273e8b066f70c206551210d902b"
+            albumTitle="Bohemian Rhapsody"
+            albumHref="https://open.spotify.com/track/3z8h0TU7ReDPLIbEnYhWZb"
+            playlistHref="https://open.spotify.com/album/6i6folBtxKV28WX3msQ4FE"/>
+        </div>
+      </div>
     </div>
+    
+
   );
 }
 
---
+++ src/components/components.js
@@ -0,0 +1,23 @@
+import data from '../albumData';
+
+
+
+const UserInfo = ({ avatarSrc, name }) => (
+    <div className="user-info">
+      <img className="avatar" alt="avatar" src={avatarSrc} />
+      <p>{name}</p>
+    </div>
+  );
+
+const MyExtractingAfter = ({ artistName, nameHref, albumPictSrc, albumTitle, albumHref, playlistHref}) => (
+    <div className="container album">
+        <img className="albumPict" alt="albumPict" src={albumPictSrc} />
+        <p>Title: <a className="link" href={albumHref}>{albumTitle}</a></p>
+        <p>Artist: <a className="link" href={nameHref}>{artistName}</a></p>
+        <button onClick={playlistHref}>Select</button>
+    
+    </div>
+    
+  );
+
+export default MyExtractingAfter
